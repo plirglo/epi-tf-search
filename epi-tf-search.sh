af9a027cd2f59c31e0f38878176ad9897e73fa08
Irek Glownia <ireneusz.glownia@hitachi-powergrids.com>
2021-04-21 16:12:37 +0200

    Add suggested nex public id

--- epi-tf-search.sh
+++ epi-tf-search.sh
@@ -11,10 +11,14 @@ network_security_group_id=$(grep -E '"network_security_group_id".*kubernetes' $1
 subnet_id=$(grep -E '"subnet_id".*kubernetes' $1 | head -1 | awk -F\" '{print $4}')
 public_ip_address_id=$(grep -E '"public_ip_address_id".*kubernetes.*node' $1 | awk -F\" '{print $4}')
 
+sug_last_public_ip_address_digit=$(grep -E '"public_ip_address_id".*kubernetes.*node' $1 | awk -F\" '{print $4}' | tail -n 1| grep -Eo '[0-9]+$' | awk '{print ($1)+1}')
+remove_last_char=$(grep -E '"public_ip_address_id".*kubernetes.*node' $1 | awk -F\" '{print $4}' | tail -n 1| sed 's/.$//')
+
 printf "resource_group_name:\n$resource_group_name\n\n";
 printf "location:\n$location\n\n";
 printf "network_security_group_id:\n$network_security_group_id\n\n";
 printf "subnet_id:\n$subnet_id\n\n";
 printf "public_ip_address_id:\n$public_ip_address_id\n\n";
+printf "suggested next public_ip_address_id:\n$remove_last_char$sug_last_public_ip_address_digit\n\n";
 printf "Done!\n\n";
 
